d343af3653fc39b1d4244f7530491e9abf404b76
Your Name <you@example.com>
2018-12-08 13:49:52 +0400

    fjeiorjferf

--- index.js
+++ index.js
@@ -41,9 +41,55 @@
 
 
 
-app.get("")
-app.get("/name/name", function (req, res) {
-    var name = req.params.name;
-    res.send("<h1>Hello " + name +  "</h1>");
+// var Square = require("./module");
+// var mySquareObject = new Square(5);
 
-});
+// function main() {
+//    console.log(mySquareObject.getArea(5));
+// }
+// main(); 
+/////////////////////////////////
+// var fs = require('fs');
+
+// function main(){
+//    var file  = "hello.txt";
+//    fs.appendFileSync(file, "Hello world\n");
+// }
+// main();
+
+// var fs = require('fs');
+
+// function main(){
+//    fs.writeFile("hello.txt", "hello world\n",function(err){
+//    console.log("fs.writeFile ended");
+// });
+// console.log("fs.writeFile");
+
+// }
+// main();
+// var fs = require('fs');
+// var dummyText = "Apple yep";
+
+// function main() {
+//    fs.writeFileSync("dummytext.txt", dummyText);
+//    var text = fs.readFileSync("dummytext.txt").toString();
+//    console.log(dummyText == text);
+//    console.log(text);
+//    fs.writeFileSync("undummytext.txt",
+//        text.replace("Apple", "Microsoft")
+//    );
+// }
+// main();
+
+var fs = require('fs');
+var dummyText = "obj";
+var obj = 
+{ 
+  "first_name": "Vardan",
+  "last_name": "Hovsepyan",
+  "age": 13,
+  "tumo_student":true
+}
+var myJSON = JSON.stringify(obj);
+fs.writeFileSync("obj.json",dummyText);
+
---
+++ module.js
@@ -0,0 +1,8 @@
+module.exprots = class  Square {
+    constructor(side){
+        this.side = side;
+    }
+    getArea() {
+        return this.side * this.side;
+    }
+};
